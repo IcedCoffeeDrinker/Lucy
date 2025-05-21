2e3b27a443c2e17fe3a7c44d719fad12eef61b25
IcedCoffeeDrinker <philipp.haus@icloud.com>
2025-05-21 21:41:22 +0200

    added centralized uvicorn

---
+++ src/services/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from src.services.database.main import app as database_app
+from src.services.ollama_api.main import app as ollama_app
+from src.services.twilio_api.main import app as twilio_app
+app = FastAPI()
+
+# already specified path in the imported apps, e.g. /ollama_api
+app.mount("/", database_app)
+app.mount("/", ollama_app)
+app.mount("/", twilio_app)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("project_main:app", host="0.0.0.0", port=5001, reload=True)
